45156f281b600acadce781c7173506a96652fa2c
Adrien Roches <adrien@roc-it.tech>
2025-11-18 16:49:04 +0100

    Arrange, Act, Assert (AAA) + use only what you need in tests

--- tests/Membership/MemberTest.php
+++ tests/Membership/MemberTest.php
@@ -11,22 +11,49 @@ class MemberTest extends TestCase
 {
     public function testCounterIsIncremented(): void
     {
+        // Preconditions
         $this->assertSame(0, Member::getCount());
 
+        // Arrange + Act 1
         $member1 = $this->createMember();
         $member2 = $this->createMember();
 
+        // Assert 1
         $this->assertSame(2, Member::getCount());
 
+        // Arrange + Act 2
         unset($member1);
 
+        // Assert 2
         $this->assertSame(1, Member::getCount());
     }
 
-    private function createMember(): Member
+    public function testCanBeCastedToString(): void
     {
+        // Arrange
+        $member = $this->createMember(
+            name: 'John Smith',
+            age: 60,
+        );
+
+        // Act
+        $memberAsString = (string) $member;
+
+        // Assert
+        $this->assertSame("'John Smith' (age: 60)", $memberAsString);
+    }
+
+    private function createMember(
+        string|null $name = null,
+        int|null $age = null,
+    ): Member {
         $faker = Factory::create();
 
-        return new Member($faker->name(), $faker->userName(), $faker->password(), random_int(22, 89));
+        return new Member(
+            $name ?? $faker->name(),
+            $faker->userName(),
+            $faker->password(),
+            $age ?? random_int(22, 89)
+        );
     }
 }
